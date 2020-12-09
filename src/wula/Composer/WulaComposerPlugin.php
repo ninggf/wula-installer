999f2886ef1cf96b08505cf00573732ab043a602
windywany <windywany@163.com>
2020-12-09 10:04:46 +0800

    兼容2.0

--- src/wula/Composer/WulaComposerPlugin.php
+++ src/wula/Composer/WulaComposerPlugin.php
@@ -7,9 +7,20 @@ use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
 
 class WulaComposerPlugin implements PluginInterface {
-	public function activate(Composer $composer, IOInterface $io) {
-		$installer = new WulaInstaller($io, $composer);
-		$im        = $composer->getInstallationManager();
-		$im->addInstaller($installer);
-	}
+    private $installer;
+
+    public function activate(Composer $composer, IOInterface $io) {
+        $installer = new WulaInstaller($io, $composer);
+        $im        = $composer->getInstallationManager();
+        $im->addInstaller($installer);
+        $this->installer = $installer;
+    }
+
+    public function deactivate(Composer $composer, IOInterface $io) {
+        #理论上不能停用
+    }
+
+    public function uninstall(Composer $composer, IOInterface $io) {
+        #$composer->getInstallationManager()->removeInstaller($this->installer);
+    }
 }
